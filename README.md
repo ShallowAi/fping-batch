ee4d0ac76fac153b35db97080f9573506907ee80
ShallowAi <1402320551@qq.com>
2020-02-23 15:06:44 +0800

    Fixed typo, added new method

--- README.md
+++ README.md
@@ -2,10 +2,13 @@
 fping batch process script with output file.
 
 ## What's this?
-This is an batch linux script for fping.
+This is an batch linux script for fping, export csv table for analyze.
 
 ## How to use?
 1. Install fping.
 2. Download script.
-3. Put your ip file in the same folder and named it as ips.txt.
+3. Put your ip list file in the same folder and change it name as ips.txt.
 4. Run script.
+
+> It worse than you think.
+> LICENSE: Apache-2.0
--- ping.sh
+++ ping.sh
@@ -1,27 +1,92 @@
 #!/bin/bash
 # author:ShallowAi
-# Set ips.txt split part by how many lines. (Default: false)
-count=1000000
-# Separate output csv. (Default: 1, Set 0 to enable)
-sepa=1
-echo INFO: IP Pages have been Separated to $count parts. Output csv separate is $sepa.
-echo WARN: Please not decrease Count value too low!
-echo WARN: It will cost more performance and network traffic.
-echo WARN: 
-split -$count ips.txt ip_
-for file in `ls ip_*`;
+
+# Set ips.txt split part by how many line.
+echo 'Please set ip list file split line count.(Default: 100000)'
+echo 'WARN: Please not decrease Count value too low!'
+echo 'WARN: It will cost more performance and network traffic. May cause inaccurate.'
+read spl
+while (($spl<=0))
 do
-	fping -A -e -r 5 -f $file > pingdata_$file.tmp &
-done;
-echo INFO: ALL fping process create completed. Please wait until ping complete.
+	echo 'ERROR: invalid split count number. Set to Default'
+	spl=100000
+done
+
+# Separate output csv.
+echo 'Separate output csv file? (Default: false)'
+read sepa
+case $sepa in
+	true) echo 'SET: true'
+	;;
+	false) echo 'SET: false'
+	;;
+	*) sepa=false
+	;;
+esac
+echo 'INFO: IP Pages have been Separated by $spl line every parts. Output csv separate is $sepa.'
+
+# Ping Method
+echo 'Which ping method do you prefer? (fast/exact/custom)'
+echo 'fast: Send 1 pings to target, output ping only.(Default)'
+echo 'exact: Send 4 pings to target, output min/avg/max ping and packet loss.'
+echo 'custom: Send custom numbers pings to target. output min/avg/max ping and packet loss.'
+read method
+case $method in
+	fast) echo 'You choosed fast method.'
+	count=1
+	;;
+	exact) echo 'You choosed exact method.'
+	count=4
+	;;
+	custom) echo 'You choosed custom method. Please type your numbers.'
+	read count
+	;;
+esac
+
+# ip split part
+split -$spl ips.txt ip_
+# Ping execute part.
+case $count in
+	1)
+	for file in `ls ip_*`;
+	do
+	fping -A -e -f $file > pingdata_$file.tmp &
+	done;
+	;;
+	*)
+	for file in `ls ip_*`;
+	do
+	fping -A -c $count -q -e -f $file &> pingdata_$file.tmp &
+	done;
+	;;
+esac
+
+echo 'INFO: ALL fping process create completed. Please wait until ping complete.'
 wait
-echo INFO: ALL fping ping completed. Ready to export csv file.
-for file in `ls pingdata_*.tmp`;
-do
-	sed -i 's/ is alive (/,/g; s/ ms)/,/g' $file.tmp
-	if [sepa=1]
-	then
-		cat $file.tmp >> output.csv
-		rm -rf $file.tmp
-	fi
-done;
+
+# Output process part
+echo 'INFO: ALL fping ping completed. Ready to export csv file.'
+case $count in
+	1)
+		for file in `ls pingdata_*.tmp`;
+		do
+		sed -i 's/ is alive (/,/g; s/ ms)/,/g; s/ is unreachable/,500,/g' $file
+		if [sepa=false]
+			then
+			cat $file >> output.csv
+			rm -rf $file
+		fi
+		done;
+	;;
+	*)
+		for file in `ls pingdata_*.tmp`;
+		do
+		sed -i "s/' : xmt/rcv/%loss = 4/4/'/,/g; s/', min/avg/max = '/,/g; s/'/'/,/g" $file
+		if [sepa=false]
+			then
+			cat $file >> output.csv
+			rm -rf $file
+		fi
+		done;
+	;;
+esac
